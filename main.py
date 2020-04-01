a9254540183dafdcf234def491035f13cac68710
ConorMeegan <conmeegan@gmail.com>
2020-04-01 16:48:29 +0100

    Setting up main function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import tensorflow
 
+
+def main():
+    print("Deep Learning Project - Group 16"
+          "\n--------------------------------"
+          "\nConor Meegan - 16347531 "
+          "\nHugh Ormond - 16312941"
+          "\nStephen Doughten - 19203845"
+          "\n--------------------------------")
+
+
+if __name__ == '__main__':
+    main()
