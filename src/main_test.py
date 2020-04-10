bc7e0095ce36a32a59344eea7aa3fb1bdf2acee2
StephenDoughten <63006539+stephedoughten@users.noreply.github.com>
2020-04-10 16:14:07 +0100

    Update main_test.py

--- src/main_test.py
+++ src/main_test.py
@@ -26,19 +26,37 @@ def main():
     #print(test_text)
     #test_text = re.sub('@[A-Za-z0-9_]+', '', str(test_text))
     #print(test_text)
-    for i in x_data.index:
-        #remove @s twitter only allows alphanumeric and underscores in their names
-        x_data.loc[i, 'text'] = re.sub('@[A-Za-z0-9_]+', '', str(x_data.loc[i, 'text']))
-        #remove all URLs, difficult to glean any meaning from them
-        x_data.loc[i, 'text'] = re.sub('http[A-Za-z0-9_:/.]+', '', str(x_data.loc[i, 'text']))
-        #print(x_data.loc[i, 'text'])
-        if i%100 == 0:
-            print(i)
+    num_rows=x_data.count()
+    num_rows=int(num_rows[0])
+    print(num_rows)
+    x_data2= x_data.iloc[0:0]
+    x_test = x_data.head(10001).copy()
+    for j in range(0, int(num_rows/1000)):
+        first = j*1000
+        last = (j+1)*1000-1
+        print(last)
+        x_test = x_data.iloc[first: last].copy()
+        for i in range(first, last):
+            #remove @s twitter only allows alphanumeric and underscores in their names
+            x_test.loc[i, 'text'] = re.sub('@[A-Za-z0-9_]+|http[A-Za-z0-9_:/\.]+', '', str(x_test.loc[i, 'text']))
+            if i%1000 == 0:
+                print(i)
+    x_data2.append(x_test)
 
 
-    x_data.to_csv('Training_xData')
-    y_label.to_csv('Training_yData')
-    print(x_data.loc[500, 'text'])
+   # for i in x_test.index:
+   #     #remove @s twitter only allows alphanumeric and underscores in their names
+   #     x_test.loc[i, 'text'] = re.sub('@[A-Za-z0-9_]+', '', str(x_test.loc[i, 'text']))
+   #     #remove all URLs, difficult to glean any meaning from them
+   #     x_test.loc[i, 'text'] = re.sub('http[A-Za-z0-9_:/.]+', '', str(x_test.loc[i, 'text']))
+   #     #print(x_data.loc[i, 'text'])
+   #     if i%100 == 0:
+   #         print(i)
+
+
+    #x_data.to_csv('Training_xData')
+    #y_label.to_csv('Training_yData')
+ #   print(x_data.loc[500, 'text'])
 
 if __name__ == '__main__':
     main()
