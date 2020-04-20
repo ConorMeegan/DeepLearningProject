a770aa14c38bed731dba34d5941ce9445be7245c
Hugh Ormond <hghrmnd@gmail.com>
2020-04-20 21:44:16 +0100

    Update preprocessing

--- src/preprocessing.py
+++ src/preprocessing.py
@@ -6,21 +6,19 @@ import re
 import string
 
 
-def main():
+class ProcessText():
 	cols = ['target', 'id', 'date', 'flag', 'user', 'text']
     tweets = pd.read_csv('training.csv', header=None, names=cols,
                          encoding='latin-1')
 						 
 	text = tweets.drop('target', axis=1)
 	
-	
-def ProcessText(text):
 	def Clean_ats_and_link(text):
 		#remove @s twitter only allows alphanumeric and underscores in their names
 		return re.sub('@[A-Za-z0-9_]+|http[A-Za-z0-9_:/\.]+', '', str(text))
 	
 	def clean_digits(text):
-		return re.sub('\d+', '', input_text)
+		return re.sub('\d+', '', text)
 		
 	def remove_stopwords(text):
 		stopword_list = stopwords.words('english')
@@ -30,3 +28,5 @@ def ProcessText(text):
 		cleaned_words = [word for word in words if (word not in stopword_list or word in skip_words) and len(word) > 1]
 		
 		return " ".join(cleaned_words)
+		
+	
